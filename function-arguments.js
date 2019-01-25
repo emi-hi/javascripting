6a09211ab549009e6875e8f89d0415eb9c2caa63
emi-hi <emily.hillier@gmail.com>
2019-01-25 02:52:55 +0000

    all of my initial work so far

---
+++ function-arguments.js
@@ -0,0 +1,5 @@
+function math(a,b,c){
+  console.log((b*c) + a)
+}
+
+math(53, 61, 67)
---
+++ looping-through-arrays.js
@@ -0,0 +1,4 @@
+var pets = ['cat', 'dog', 'rat'];
+for (var i = 0; i < pets.length; pets[i] = pets[i] + 's', i++){
+}
+console.log(pets)
